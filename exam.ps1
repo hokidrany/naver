d24c314b9b8d50797090c64d19de82d6cf8aa4c8
kokidrany <kokidrany@naver.com>
2021-12-03 21:33:10 +0900

    .

--- exam.ps1
+++ exam.ps1
@@ -1,6 +1,130 @@
 clear
-$PSScriptRoot
+#switch
+$values = "Start","MoveNext","NotEvaluated","Reset","End"
+$stopInfinite = $false
+switch($values){
+    "MoveNext"{
+        "`tMoveNext"
+        $switch.MoveNext()  | Out-Null
+        "`tAfter MoveNext: $($switch.Current)"
+    }
+    "NotEvaluated"{
+        "`t2AfterMoveNext: $($switch.Current)"
+    }
+    "Reset"{
+        if(!$stopInfinite){
+            "`tReset"
+            $switch.Reset()
+            $stopInfinite = $true
+        }
+    }
+    default{
+        "Default (Current): $($switch.Current)"
+    }
+}
+exit
 
+#foreach 와 reset()
+$stopLoop = 0
+foreach ($num in ("one","two","three")){
+    ("`t" * $stopLoop) + "Current: $($foreach.Current)"
+    if($num -eq "two" -and $stopLoop -lt 2){
+        $foreach.reset() | Out-Null
+        ("`t" * $stopLoop) + "Reset Loop: $stopLoop"
+        $stopLoop++
+    }
+}
+exit
+
+#foreach 와 foreach.current
+$i = 0
+foreach($num in ("one","two","three")){
+    "Iteration: $i"
+    $i++
+    "`tNum: $num"
+    "`tCurrent: $($foreach.Current)"
+    if($foreach.Current -eq "two"){
+        "Before MoveNext (Current): $($foreach.Current)"
+        $foreach.MoveNext()| Out-Null
+        "After MoveNext (Current): $($foreach.Current)"
+        "Num has not changed: $num"
+    }
+}
+exit
+
+
+#$input Current 속성 사용하기
+function Current{
+    begin{
+        $i = 0
+    }
+    process{
+        "Iteration: $i"
+        $i++
+        "`tBefore MoveNext:$($input.Current)"
+        $input.MoveNext() | Out-Null
+        "`tAfter MoveNext: $($input.Current)"
+        "`tAccess Again: $($input.Current)"
+    }
+}
+"one","two" | Current
+exit
+function All{
+    "All values: $input"
+    "Access again: $input"
+    $input.reset()
+    "After reset: $input"
+    $input.MoveNext() | Out-Null
+    "After MoveNext: $input"
+}
+"one","two","three" | All
+exit
+
+
+
+#$input $skip
+$skip = $true
+function Skip{
+    begin{
+        $i = 0
+    }
+    process{
+        "Iteration: $i"
+        $i++
+        if($skip){
+            "`tSkipping"
+            $skip=$false
+        }
+        else{
+            "`tInput: $input"
+        }
+    }
+}
+"one","two","three" | Skip
+exit
+
+function Test{
+    Begin{
+        $i = 0
+    }
+    process{
+        "Iteration: $i"
+        $i++
+        "`tInput: $input"
+        "`tAccess Again: $input"
+        $input.Reset()
+        "`tAfter Reset: $input"
+    }
+}
+"one","two" | Test
+exit
+
+
+
+
+
+
+$PSScriptRoot
 Select-String -Pattern "Test" -Path .\exam.ps1
 $PSCulture
 Get-Culture
