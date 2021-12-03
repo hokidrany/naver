31592e3331dcd6de55c4c33633b3a3d571a8cc46
kokidrany <kokidrany@naver.com>
2021-12-03 15:45:55 +0900

    powershell

---
+++ exam.ps1
@@ -0,0 +1,40 @@
+clear
+$PSScriptRoot
+Select-String -Pattern "Test" -Path .\exam.ps1
+$PSCulture
+Get-Culture
+function Test1 {
+    param($a, $b) 
+            $PSBoundParameters      
+}
+
+function test2 {
+    param (
+        $a,$b
+    )
+    Test1 @PSBoundParameters
+}
+Test2 -a Power -b Shell
+$PSCommandPath
+Exit
+
+
+
+Test-Path $profile
+$PID
+"======================="
+$a = "one", $null, "three"
+$a.Count
+$a | ForEach-Object { "Hello" + $_}
+
+$calendar = @($null, $null, "Meeting", $null, $null, "Team Lunch", $null)
+$days = "Sunday","Monday","Wednesday","Thursday","Friday","Saturday"
+#$calendar
+#$days
+$currentDay = 0
+foreach($day in $calendar){
+    if($day -ne $null){
+        "Appointment on $($days[$currentDay]): $day"
+    }
+    $currentDay++
+}
